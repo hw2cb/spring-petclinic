0ac2c1235188eccfeacd5d5b4d57bad02efedceb
hw2cb <62744666+hw2cb@users.noreply.github.com>
2021-11-30 16:06:39 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 set M2_HOME=C:\apache-maven-3.8.4
 set path=C:\apache-maven-3.8.4\bin;%path%;
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
+set path=C:\Program Files\Java\jdk1.8.0_202\bin;%path%;
 mvn package
